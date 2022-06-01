4651d18e23681cdcb35075e7b239b0f937441565
Pshemak <przemek1441@o2.pl>
2022-06-01 11:57:46 +0200

    fileStructure

---
+++ server.js
@@ -0,0 +1,21 @@
+const { application } = require("express");
+var express = require("express")
+var app = express()
+var path = require("path")
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.text())
+
+app.use(express.static('static'))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/static/index.html"))
+})
+
+app.listen(PORT, function () {
+    console.log("start serwera na porcie " + PORT)
+})
+
+
---
+++ static/scripts/classes/Main.js
@@ -0,0 +1,4 @@
+
+const loadWholeGame = () => {
+    console.log("XD")
+}
