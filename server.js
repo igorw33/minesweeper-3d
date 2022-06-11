1c74bbf63a594806ee481deb1da8e2cfa841e1d4
igorw33 <igorwalasek@onet.pl>
2022-06-11 15:00:30 +0200

    basics

--- server.js
+++ server.js
@@ -10,9 +10,75 @@ app.use(express.text())
 
 app.use(express.static('static'))
 
+
+let users = []
+let lastChange = {}
+let newMove = {}
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/static/index.html"))
-    console.log("test")
+})
+
+// Dodawanie usera - kliknięcie przycisku loguj na stronie
+// Po udanym zalogowaniu na kliencie wyślij POST /CHECK_USERS
+
+app.post("/ADD_USER", (req, res) => {
+    let userInfo = req.body
+    if (users.length >= 2) {
+        console.log("nie można dodać - za dużo userów")
+        res.send(JSON.stringify("error - nie można dodać, za dużo userów", null, 5))
+    }
+    else {
+        if (users.length == 0) {
+            users.push(userInfo.userName)
+            console.log(users)
+            res.send(JSON.stringify(users, null, 5));
+        }
+        else if (users.length == 1) {
+            if (users[0] == userInfo.userName) {
+                console.log("nie można dodać - jest już taki username")
+                res.send(JSON.stringify("error - nie można dodać, jest już tak username", null, 5))
+            }
+            else {
+                users.push(userInfo.userName)
+                console.log(users)
+                res.send(JSON.stringify(users, null, 5));
+            }
+        }
+    }
+})
+
+// Sprawdzanie ilości Userów
+
+app.post("/CHECK_USERS", (req, res) => {
+    console.log("sprawdzam ilość userów...")
+    console.log(users)
+    if (users.length == 2) {
+        // Koniec czekania, początek gry
+        res.send(JSON.stringify("Koniec", null, 5))
+    }
+    else {
+        // Ekran czekania
+        res.send(JSON.stringify("Czekaj na drugiego gracza", null, 5))
+    }
+})
+
+// Otrzymanie nowego ruchu
+
+app.post("/NEW_MOVE", (req, res) => {
+    newMove = req.body
+    res.send(JSON.stringify(newMove))
+})
+
+// Sprawdzanie ostatniego ruchu, wykonuje się ciągle
+
+app.post("/MOVE_CHECK", (req, res) => {
+    if (JSON.stringify(newMove) != JSON.stringify(lastChange)) {
+        lastChange = newMove
+        res.send(JSON.stringify(lastChange))
+    } else {
+        res.send(null)
+    }
 })
 
 app.listen(PORT, function () {
